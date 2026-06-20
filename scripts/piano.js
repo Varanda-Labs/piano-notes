59e43261ad8d4fb86518a75584aa8b342b119335
Marcelo Varanda <m@varanda.ca>
2026-06-20 12:27:45 -0400

    initial midi input integration (missing key up, pedal handling and note draw)

--- scripts/piano.js
+++ scripts/piano.js
@@ -31,6 +31,19 @@ const BALL = 70;
 const BLACK_NOTE_POS_Y = BLACK_NOTE_H - BALL;
 const WHITE_NOTE_POS_Y = KEY_H - BALL;
 
+const ALL_NOTES_NAMES = [
+  'A0', 'A#0', 'B0', 'C1', 'C#1', 'D1', 'D#1', 'E1', 'F1', 'F#1', 'G1', 'G#1',
+  'A1', 'A#1', 'B1', 'C2', 'C#2', 'D2', 'D#2', 'E2', 'F2', 'F#2', 'G2', 'G#2',
+  'A2', 'A#2', 'B2', 'C3', 'C#3', 'D3', 'D#3', 'E3', 'F3', 'F#3', 'G3', 'G#3',
+  'A3', 'A#3', 'B3', 'C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4',
+  'A4', 'A#4', 'B4', 'C5', 'C#5', 'D5', 'D#5', 'E5', 'F5', 'F#5', 'G5', 'G#5',
+  'A5', 'A#5', 'B5', 'C6', 'C#6', 'D6', 'D#6', 'E6', 'F6', 'F#6', 'G6', 'G#6',
+  'A6', 'A#6', 'B6', 'C7', 'C#7', 'D7', 'D#7', 'E7', 'F7', 'F#7', 'G7', 'G#7',
+  'A7', 'A#7', 'B7', 'C8'
+]
+
+const MIDI_FIRST_NOTE_OFFSET = 21
+
 const BLACK_NOTES_X = [
         131, 407, 589, 868, 1036, 1203,
         1479, 1661, 1940, 2108, 2275,
@@ -150,49 +163,6 @@ const CANVAS_STATE = {
 
 var midi = null;
 
-// Main MIDI listener
-(async () => {
-    try {
-        midi = await navigator.requestMIDIAccess();
-        console.log('🎹 MIDI devices available:', midi.devices);
-
-        midi.inputs.forEach(port => {
-            port.onmidimessage = message => {
-                const [command, note, velocity] = message.data;
-
-                // Determine message type
-                const commandType = (command & 0xF0) === 0x90 ? 'Note On' :
-                                  (command & 0xF0) === 0x80 ? 'Note Off' :
-                                  (command & 0xF0) === 0xA0 ? 'Aftertouch' :
-                                  'Other';
-
-                if ((command & 0xF0) !== 0x90) return; // only Note On with velocity > 0
-                if (velocity == 0) return;
-
-                // Get note name
-                const noteNames = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
-                const noteName = noteNames[note % 12] + (Math.floor(note / 12) - 1);
-
-                console.log(
-                    `[${commandType}] Note: ${noteName}, Pitch: ${note}, Velocity: ${velocity}`
-                );
-            };
-        });
-
-        // Handle MIDI state changes
-        midi.onstatechange = event => {
-            if (event.state === 'disconnected') {
-                console.log('🔌 MIDI disconnected');
-                connectBtn.textContent = 'Reconnect';
-                connectBtn.disabled = false;
-            }
-        };
-    } catch (err) {
-        //console.error('❌ MIDI Access Error:', err.message);
-    }
-})();
-
-
 class Piano extends Instrument{
   constructor(canvas, statusDisplay, pedalDownCheckBox, noteStrokeCallback = null){
     super(noteStrokeCallback);
@@ -211,7 +181,58 @@ class Piano extends Instrument{
     this.pedalDownCheckBox.addEventListener('change', (event) => this.onPedalChange(event));
     addEventListener("midimessage", (event) => this.onMidiMessage(event));
 
+    this.initMidi();
     this.Repaint();
+  }
+
+  initMidi() {
+    // Main MIDI listener
+    (async () => {
+        try {
+            midi = await navigator.requestMIDIAccess();
+            console.log('🎹 MIDI devices available:', midi.devices);
+
+            midi.inputs.forEach(port => {
+                port.onmidimessage = message => {
+                    const [command, note, velocity] = message.data;
+
+                    // Determine message type
+                    const commandType = (command & 0xF0) === 0x90 ? 'Note On' :
+                                      (command & 0xF0) === 0x80 ? 'Note Off' :
+                                      (command & 0xF0) === 0xA0 ? 'Aftertouch' :
+                                      'Other';
+
+                    if ((command & 0xF0) !== 0x90) return; // only Note On with velocity > 0
+                    if (velocity == 0) return;
+
+                    // Get note name
+                    //const noteNames = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+                    //const noteName = noteNames[note % 12] + (Math.floor(note / 12) - 1);
+                    var noteName = ALL_NOTES_NAMES[note - MIDI_FIRST_NOTE_OFFSET];
+                    console.log(`this.canvas_piano: ${this.canvas_piano}` );
+                    console.log(`**** noteName: ${noteName}` );
+
+                    this.playNote(noteName);
+
+
+                    console.log(
+                        `[${commandType}] Note: ${noteName}, Pitch: ${note}, Velocity: ${velocity}`
+                    );
+                };
+            });
+
+            // Handle MIDI state changes
+            midi.onstatechange = event => {
+                if (event.state === 'disconnected') {
+                    console.log('🔌 MIDI disconnected');
+                    connectBtn.textContent = 'Reconnect';
+                    connectBtn.disabled = false;
+                }
+            };
+        } catch (err) {
+            //console.error('❌ MIDI Access Error:', err.message);
+        }
+    })();
   }
 
   Repaint() {      
