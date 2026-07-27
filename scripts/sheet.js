96bdc5e5c5f6771e09dccf6f65e0f2078c80a124
Marcelo Varanda <m@varanda.ca>
2026-07-27 10:23:52 -0400

    show good emoji

--- scripts/sheet.js
+++ scripts/sheet.js
@@ -18,6 +18,41 @@ import { Staff, G_CLEF, F_CLEF } from "./staff.js";
 
 const WIDTH_SCALE_REF = 1400;
 
+const GOOD_IMG_PATH = './res/emojis-up/';
+const BAD_IMG_PATH = './res/emojis-down/';
+
+const emoji_good = [
+  'emoji_u1f60a.svg',
+  'emoji_u1f60b.svg',
+  'emoji_u1f60c.svg',
+  'emoji_u1f60d.svg',
+  'emoji_u1f60e.svg',
+  'emoji_u1f60f.svg',
+  'emoji_u1f61a.svg',
+  'emoji_u1f61b.svg',
+  'emoji_u1f61c.svg',
+  'emoji_u1f61d.svg',
+  'emoji_u1f63a.svg',
+  'emoji_u1f63b.svg',
+  'emoji_u1f63c.svg',
+  'emoji_u1f63d.svg',
+];
+
+const emoji_bad = [
+  'emoji_u1f61e.svg',
+  'emoji_u1f61f.svg',
+  'emoji_u1f62a.svg',
+  'emoji_u1f62b.svg',
+  'emoji_u1f62c.svg',
+  'emoji_u1f62d.svg',
+  'emoji_u1f62e_200d_1f4a8.svg',
+  'emoji_u1f62e.svg',
+  'emoji_u1f62f.svg',
+  'emoji_u1f63e.svg',
+  'emoji_u1f63f.svg',
+  'emoji_u1f64a.svg',
+];
+
 class Sheet {
   constructor(canvas, goodAnimationDoneCallback = null, badAnimationDoneCallback = null) {
     this.canvas = canvas;
@@ -30,6 +65,9 @@ class Sheet {
     this.staff_2 = new Staff(canvas_notes, F_CLEF);
 
     this.current_note = null; //this.staff_2.addNote('C2', false);
+
+    this.goodImage = null;
+    this.badImage = null;
 
     this.Repaint();
   }
@@ -49,7 +87,7 @@ class Sheet {
   }
 
   Repaint() {
-    this.scale = this.canvas.width / WIDTH_SCALE_REF;
+    //this.scale = this.canvas.width / WIDTH_SCALE_REF;
     const canvas_h = window.innerHeight / 2;
     this.canvas.height = canvas_h;
     this.scale = this.canvas.width / WIDTH_SCALE_REF;
@@ -73,20 +111,40 @@ class Sheet {
 
   }
 
+  getRandom(exclusive_max) {
+    return Math.floor(Math.random() * exclusive_max);
+  }
+
   goodAnimationDone(event) {
     if (this.goodAnimationDoneCallback != null) {
       this.goodAnimationDoneCallback();
     }
+    this.goodImage = null;
   }
 
   badAnimationDone(event) {
     if (this.badAnimationDoneCallback != null) {
       this.badAnimationDoneCallback();
     }
+    this.badImage = null;
   }
 
   startGoodAnimation() {
-    setTimeout(event =>this.goodAnimationDone(event), 1000);
+    this.goodImage = new Image();
+    this.goodImage.onload = () => {
+      const ctx = this.canvas.getContext('2d');
+      ctx.drawImage(  this.goodImage,
+                      0,
+                      0,
+                      100, 
+                      100);
+
+      setTimeout(event =>this.goodAnimationDone(event), 1000);
+
+    }
+    this.goodImage.src = GOOD_IMG_PATH + emoji_good[0];
+
+    // setTimeout(event =>this.goodAnimationDone(event), 1000);
   }
 
   startBadAnimation() {
